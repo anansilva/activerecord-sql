63169e635ed3468f98ddbcf0c41dcc7cf966d05c
Ana NS <ananunesdasilva@gmail.com>
2021-03-05 16:59:19 +0000

    improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,38 @@
+puts 'pruning all data'
+Payment.destroy_all
+Rating.destroy_all
+Accommodation.destroy_all
+Booking.destroy_all
+Host.destroy_all
+Guest.destroy_all
+
 def calculate_number_of_nights(check_in, check_out)
   (check_out.to_date - check_in.to_date).to_i
 end
 
-def calculate_total_price(accommodation, check_in, check_out)
-  accommodation.price_per_night * calculate_number_of_nights(check_in, check_out)
+def calculate_total_price(total_guests, accommodation, check_in, check_out)
+  total_guests * accommodation.price_per_night * calculate_number_of_nights(check_in, check_out)
 end
 
 def random_accommodation
   Accommodation.order('Random()').first
 end
 
-puts 'pruning all data'
-Accommodation.destroy_all
-Booking.destroy_all
-Host.destroy_all
-Guest.destroy_all
+def past_random_check_in
+  rand(1..5).months.ago
+end
+
+def past_random_check_out
+  past_random_check_in + rand(2..10).days
+end
+
+def future_random_check_in
+  rand(1..5).months.from_now
+end
+
+def future_random_check_out
+  future_random_check_in + rand(1..10).days
+end
 
 puts 'creating hosts'
 3.times do
@@ -53,16 +71,14 @@ RANDOM_COMMENTS = {
 
 2.times do
   Guest.create(name: Faker::Name.name).tap do |guest|
-    random_check_in = rand(1..5).months.ago
-    random_check_out = random_check_in + rand(2..10).days
-
     accommodation = random_accommodation
-    total_price = calculate_total_price(accommodation, random_check_in, random_check_out)
+    total_guests = rand(1..4)
+    total_price = calculate_total_price(total_guests, accommodation, past_random_check_in, past_random_check_out)
 
     Booking.create(guest_id: guest.id,
                    accommodation_id: accommodation.id,
-                   check_in: random_check_in, check_out: random_check_out,
-                   total_guests: rand(1..4), status: 4).tap do |booking|
+                   check_in: past_random_check_in, check_out: past_random_check_out,
+                   total_guests: total_guests, status: 4).tap do |booking|
 
                      Payment.create(booking_id: booking.id, amount: total_price,
                                    status: 1)
@@ -78,19 +94,14 @@ end
 puts 'creating guests with past and future bookings'
 2.times do
   Guest.create(name: Faker::Name.name).tap do |guest|
-    past_random_check_in = rand(1..5).months.ago
-    past_random_check_out = past_random_check_in + rand(2..10).days
-
-    future_random_check_in = rand(1..5).months.from_now
-    future_random_check_out = future_random_check_in + rand(1..10).days
-
     accommodation = random_accommodation
-    total_price = calculate_total_price(accommodation, past_random_check_in, past_random_check_out)
+    total_guests = rand(1..4)
+    total_price = calculate_total_price(total_guests, accommodation, past_random_check_in, past_random_check_out)
 
     Booking.create(guest_id: guest.id,
                    accommodation_id: accommodation.id,
                    check_in: past_random_check_in, check_out: past_random_check_out,
-                   total_guests: rand(1..4), status: rand(0..2)).tap do |booking|
+                   total_guests: total_guests, status: rand(0..2)).tap do |booking|
                      Payment.create(booking_id: booking.id, amount: total_price,
                                    status: 1)
                      rate = rand(1..5)
@@ -99,9 +110,15 @@ puts 'creating guests with past and future bookings'
                    end
 
     accommodation = random_accommodation
+    total_guests = rand(1..4)
+    total_price = calculate_total_price(total_guests, accommodation, future_random_check_in, future_random_check_out)
+
     Booking.create(guest_id: guest.id,
                    accommodation_id: accommodation.id,
                    check_in: future_random_check_in, check_out: future_random_check_out,
-                   total_guests: rand(1..4), status: 4)
+                   total_guests: total_guests, status: 4).tap do |booking|
+                     Payment.create(booking_id: booking.id, amount: total_price,
+                                   status: 0)
+                   end
   end
 end
